a1f33fb57addf88bc834c2437b74b308810159ba
Pintos <epintos@trecursos.org>
2023-03-29 10:53:13 +0200

    Subida del proyecto a GitHub

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hola mi server en Express");
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send("Hola, soy un nueva ruta");
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () =>{
+  console.log('My port: ' + port);
+});
